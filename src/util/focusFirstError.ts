4228dc9a927ec127a286f47b94201a6bb567c8d4
Matthew Wong <matthew@eduka.com>
2020-07-17 15:02:21 +0800

    focusFirstError correctly checks names with []

--- src/util/focusFirstError.ts
+++ src/util/focusFirstError.ts
@@ -11,10 +11,17 @@ export const focusFirstError = (
 
   const formEls = Array.from(form.elements);
   formEls.some((el) => {
-    const name = el.attributes.getNamedItem("name")?.value;
+    let name = el.attributes.getNamedItem("name")?.value;
+
+    if (!name) {
+      return false;
+    }
+
+    name = name.endsWith("[]") ? name.slice(0, -2) : name;
 
     if (name && formErrorKeys[name]) {
       (el as HTMLElement).focus();
+      console.log(el);
       return true;
     }
 
